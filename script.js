9d73fa7caf59b9150c58d829167bc20e90088a2f
IliaZhukovskii <ilia.zhukovskii.99@mail.ru>
2022-03-29 13:42:58 +0300

    поменял переменную

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 'use strict';
 
 let text = prompt("Введите строку");
-let newStr;
+
 
 function hard(str){
   if (typeof str !== "string") {
     alert("Это не строка!");
   }
 
-  newStr = str.trim();
+  str = str.trim();
  
-  if (newStr.length >= 30){
-    newStr = newStr.substr(0, 30) + '...';
+  if (str.length >= 30){
+    str = str.substr(0, 30) + '...';
   }
-  
-  console.log(newStr);
+
+  console.log(str);
 }
 
 hard(text);
