af552e7703f14cc420877cd6a4af08b9076562b2
IliaZhukovskii <ilia.zhukovskii.99@mail.ru>
2022-03-29 13:39:16 +0300

    Готовый 4 урок

--- script.js
+++ script.js
@@ -1,41 +1,20 @@
 'use strict';
 
-let quest = confirm("Продолжить на русском языке?");
-let lang;
-if( quest == true){
-   lang = "ru";
-}else{
-   lang = "en";
+let text = prompt("Введите строку");
+let newStr;
+
+function hard(str){
+  if (typeof str !== "string") {
+    alert("Это не строка!");
+  }
+
+  newStr = str.trim();
+ 
+  if (newStr.length >= 30){
+    newStr = newStr.substr(0, 30) + '...';
+  }
+  
+  console.log(newStr);
 }
 
-
-if(lang == "ru"){
-  console.log("if:  Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Вокресенье");
-}else{
-  console.log("if:  Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday");
-}
-
-
-switch (lang){
-  case "ru":
-    console.log("switch:  Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Вокресенье");
-    break;
-  case "en":
-    console.log("switch:  Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday");  
-    break;
-}
-
-let langArray = [];
-langArray["ru"] = ["Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Вокресенье"];
-langArray["en"] = ["Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday"];
-console.log(langArray[lang]);
-
-
-let namePerson = prompt("Введите имя");
-
-let message = 
-(namePerson == 'Артем') ? "директор" : 
-(namePerson == 'Александр') ? "преподаватель" : 
-"студент";
-
-console.log(message);
+hard(text);
