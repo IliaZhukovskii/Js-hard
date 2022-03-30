b5490863a4802d6fac86ad0565c599eaf45a7d04
IliaZhukovskii <ilia.zhukovskii.99@mail.ru>
2022-03-30 18:15:03 +0300

    Урок №5 усложненное задание

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 'use strict';
 
-let text = prompt("Введите строку");
-
-
-function hard(str){
-  if (typeof str !== "string") {
-    alert("Это не строка!");
+let arr = ['25', '302', '47', '444', '123', '675', '555'];
+let num = [];
+for (let i = 0; i < arr.length; i++){
+  if(arr[i][0] == 2 || arr[i][0] == 4){
+    num.push(arr[i]);
   }
+}
+console.log("Числа, начинающиеся с цифры 2 и 4: " + num);
 
-  str = str.trim();
+
  
-  if (str.length >= 30){
-    str = str.substr(0, 30) + '...';
+nextPrime:
+for (let i = 2; i <= 100; i++) { 
+ 
+  for (let j = 2; j < i; j++) { 
+    if (i % j == 0) continue nextPrime;
   }
-
-  console.log(str);
+ 
+  console.log( i + " - Делители этого числа: 1 и " + i); // простое число
 }
 
-hard(text);
