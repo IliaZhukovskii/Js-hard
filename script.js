b71316e815a724493db93d2a67a078e81055925c
IliaZhukovskii <ilia.zhukovskii.99@mail.ru>
2022-04-01 14:55:51 +0300

    урок 7

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
 'use strict';
 
-let arr = ['25', '302', '47', '444', '123', '675', '555'];
-let num = [];
-for (let i = 0; i < arr.length; i++){
-  if(arr[i][0] == 2 || arr[i][0] == 4){
-    num.push(arr[i]);
+let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+
+let date = new Date();
+let day = date.getDay();
+
+for (let key of week) {
+
+  if (key === week[day]) {
+    key = "<b>" + key + "</b>";
+  } 
+  if (key === 'Суббота' || key === 'Воскресенье') {
+    key = "<i>" + key + "<i>";
   }
+
+  document.write(key + "<br>");
 }
-console.log("Числа, начинающиеся с цифры 2 и 4: " + num);
-
-
- 
-nextPrime:
-for (let i = 2; i <= 100; i++) { 
- 
-  for (let j = 2; j < i; j++) { 
-    if (i % j == 0) continue nextPrime;
-  }
- 
-  console.log( i + " - Делители этого числа: 1 и " + i); // простое число
-}
-
