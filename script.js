3963b5aaa74d5fcee7377f403216b119ddc687c3
IliaZhukovskii <ilia.zhukovskii.99@mail.ru>
2022-04-04 15:42:20 +0300

    исправил баг

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 'use strict';
 
-let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+let week = ['Воскресенье','Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота' ];
 
 let date = new Date();
 let day = date.getDay();
 
 for (let key of week) {
 
-  if (key === week[day]) {
+  if (key == week[day]) {
     key = "<b>" + key + "</b>";
-  } else if (key === 'Суббота' || key === 'Воскресенье') {
-    key = "<i>" + key + "<i>";
+    
+  } else if (key == 'Суббота' || key == 'Воскресенье') {
+     key = "<i>" + key + "</i>";
   }
-
   document.write(key + "<br>");
 }
